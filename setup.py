89ca2e32fa2a9c8d278c59bc4921dced0e801fb6
Jorge-Henao-Varela <jorge.varela@est.iudigital.edu.co>
2025-03-05 13:29:43 +0000

    Primer commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="actividad1",
+    version="0.0.1",
+    author="Jorge Henao Varela",
+    author_email="jorge.varela@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+) 
